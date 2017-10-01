8433c72be0d18e8ddadfcd37adf6c6358d075f98
Arul Britto <brittoprabhu@outlook.com>
2017-10-01 08:36:54 +0530

    First Commit to git

---
+++ WebApplication11/MyHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApplication11
+{
+    public class MyHub : Hub
+    {
+        public void Announce( string message)
+        {
+            Clients.All.Announce(message);
+
+        }
+
+    }
+}
---
+++ WebApplication11/Scripts/SignalR.js
@@ -0,0 +1,11 @@
+﻿
+
+(function () {
+    $.connection.hub.start()
+    .done(function () {
+
+        console.log("it works");
+        $.connection.myHub.server.announce("connected");
+    })
+    .fail(function () { alert("error!!!"); });
+    $.connection.myHub.client.announce = function (message) { $("#welcomemessage").append(message); }})()
